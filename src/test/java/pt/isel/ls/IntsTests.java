ab9e98e8cca945fcaa753348e72575d9beec7bf3
Pedro Felix <pmhsfelix@gmail.com>
2019-09-08 18:47:15 +0100

    changes checkstyle

--- src/test/java/pt/isel/ls/IntsTests.java
+++ src/test/java/pt/isel/ls/IntsTests.java
@@ -1,9 +1,8 @@
 package pt.isel.ls;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class IntsTests {
 
